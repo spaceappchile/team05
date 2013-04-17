d4878e7599d1747df1c19c7b6fa9e50e8cee81db
patricio jara <patricio@debian>
2013-04-17 20:27:10 -0300

    en el mapa sale un mensaje preguntando si activar gps

--- src/com/generic/spotapp/MapActivity.java
+++ src/com/generic/spotapp/MapActivity.java
@@ -8,20 +8,39 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
+import android.location.Criteria;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 
 public class MapActivity extends FragmentActivity {
 	
+	private static final String INFO = "INFO";
+	private static final int DIALOGO_CONFIRMACION = 0;
+	
 	private GoogleMap mMap;
+	private LatLng position;
+	private LocationListener listener;
+	private LocationManager locationManager;
+	private Criteria criteria;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		setUpMapIfNeeded();
+		gpsOn();
 	}
 	
 	@Override
@@ -37,6 +56,50 @@ public class MapActivity extends FragmentActivity {
 		return true;
 	}
 	
+	// crea los dialogos
+		protected Dialog onCreateDialog(int id) {
+		    Dialog dialogo = null;
+		 
+		    switch(id)
+		    {
+		        case DIALOGO_CONFIRMACION:
+		            dialogo = crearDialogoConfirmacion();
+		            break;
+		        
+		        //...
+		        default:
+		            dialogo = null;
+		            break;
+		    }
+		 
+		    return dialogo;
+		}
+		
+		// configuracion del mensaje/dialogo
+		private Dialog crearDialogoConfirmacion()
+		{
+		    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		 
+		    builder.setTitle("Confirmacion");
+		    builder.setMessage("¿activar GPS?");
+		    
+		    builder.setPositiveButton("Aceptar", new OnClickListener() {
+		    public void onClick(DialogInterface dialog, int which) {
+		        Log.i(INFO, "Confirmacion Aceptada.");
+		        activarGps();
+		        dialog.cancel();
+		    }
+		    });
+		    builder.setNegativeButton("Cancelar", new OnClickListener() {
+		    public void onClick(DialogInterface dialog, int which) {
+		        Log.i(INFO, "Confirmacion Cancelada.");
+		        dialog.cancel();
+		    }
+		    });
+		 
+		    return builder.create();
+		}
+	
 	private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -51,12 +114,12 @@ public class MapActivity extends FragmentActivity {
     }
 	
 	private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(30, 70)).title("Marker"));
+        showMarket(0,0,"aaaa");
         
     }
 	
 	private void showMarket(double lat, double lon, String mensaje){
-				
+		mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title(mensaje));				
 	}
 
 	private void changeView(int mode){
@@ -82,5 +145,33 @@ public class MapActivity extends FragmentActivity {
 		CameraUpdate camUpd1 = CameraUpdateFactory.newLatLng(new LatLng(lat, lon));
 		mMap.moveCamera(camUpd1);
 	}
+	public void activarGps(){
+		Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+	    startActivity(settingsIntent);		
+	}
 	
+	private void gpsOn(){
+		// para acceder a los servicios de localizacion
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		
+		// criterios
+		Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE); // localizacion exacta
+        criteria.setCostAllowed(false); 			  // sin costo monetario
+        // pregunta si hay un proveedor con estas caracteristicas
+        String providerName = locationManager.getBestProvider(criteria, true);
+        
+        // retorna true, si el gps esta activado		
+        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	
+        if(!gpsEnabled){
+			// muestra, inicia, crea, (no se que mierda) 
+			//el dialogo con el id: DIALOGO_CONFIRMACION
+			showDialog(DIALOGO_CONFIRMACION);
+			Log.d("PRUEBA GPS", "GPS apagado");
+		}
+	}
+	private void getPos(){
+	
+	}
 }
