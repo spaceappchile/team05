99a709967a606d65258a41d8ca040f6a98abb7c6
patricio jara <patricio@debian>
2013-04-21 00:05:43 -0300

    agrega una listView en main para mostrar los prox avistamientos

--- src/com/generic/spotapp/MainActivity.java
+++ src/com/generic/spotapp/MainActivity.java
@@ -4,6 +4,7 @@ package com.generic.spotapp;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -18,6 +19,7 @@ import org.json.JSONObject;
 
 import com.google.android.gcm.GCMRegistrar;
 
+
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -26,10 +28,14 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.CalendarContract.Events;
+import android.provider.ContactsContract;
 import android.provider.Settings;
+import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ListActivity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
@@ -39,23 +45,70 @@ import android.content.SharedPreferences;
 import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 
 public class MainActivity extends Activity {
 	
 	private static final String INFO="I:MainActivity";
 	private static final int DIALOGO_CONFIRMACION = 0;
+	private ArrayList<Long> longs;
+	private ArrayList<Integer> duracion;
+	private ArrayList<String> cadenas = new ArrayList();
+	
+	private ListView listView;
+		
+	// This is the Adapter being used to display the list's data
+   
+
+    // These are the Contacts rows that we will retrieve
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
+	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		duracion = new ArrayList();
+		longs = new ArrayList();
 		
+		double lat = -33.4496866030000035;
+		double lng = -70.687233315499995;
+							
 		//GCMRegistrar.checkDevice(this);
+		
+		new Proximos(1, lat, lng, this).execute();
+		
+		
+		// This is the select criteria
+		listView = (ListView) findViewById(R.id.lista_avistamientos);
+		registerForContextMenu(listView);
+				
+		
+		if(longs.size()==0)
+			Log.i(INFO,"AWEONAO, como te pasa esta wea imbecil!!!");
+		if(duracion.size()==0)
+			Log.i(INFO,"AWEONAO de la 2° forma, como te pasa esta wea imbecil!!!");
+		
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		      @Override
+		      public void onItemClick(AdapterView<?> parent, final View view, int position, long id){
+		    	  nuevoEventoCalendario(duracion.get(position), longs.get(position), view.getContext());		    	  
+		      }
+		});
+
 		
 		
 		// referencia a la coleccion de preferencias, para guardar preferencias en el dispositivo
@@ -65,19 +118,12 @@ public class MainActivity extends Activity {
 		 * Si firstTime no esta en el archivo, getBoolean devuelve el 
 		 * valor de defecto que en este caso esta dado por el segundo
 		 * parametro de la funcion.
-		 * En este caso devuelve true
-		 * 		 	
+		 * En este caso devuelve true		 	
 		 */		
-		
-		
-		
-		
-		
 		prefs.edit().clear().commit();
 		boolean firstTime = prefs.getBoolean("firstTime", true);
 		
-		if(firstTime){
-			
+		if(firstTime){			
 			Log.i(INFO,"primera vez");
 			
 			// Clase para poder realizar modificaciones en el archivo
@@ -92,7 +138,22 @@ public class MainActivity extends Activity {
 		}
 		
 	}
-
+	
+	private void crearAdapter(){
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
+		        android.R.layout.simple_list_item_1, cadenas);
+		
+		listView.setAdapter(adapter);
+	}
+        
+	// se ejecuta cuando se hace un click sobre un elemento de 
+	// la lista
+    /*
+	@Override	
+    public void onListItemClick(ListView l, View v, int position, long id) {
+    
+    }*/
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		
@@ -110,23 +171,23 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 	
-	// crea los dialogos
-		protected Dialog onCreateDialog(int id) {
-			   	Dialog dialogo = null;
+	// crea los dialogos, para el gps
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialogo = null;
 			 
-			    switch(id){
-			        case DIALOGO_CONFIRMACION:
-			            dialogo = crearDialogoConfirmacion();
-			            break;
+		switch(id){
+			case DIALOGO_CONFIRMACION:
+				dialogo = crearDialogoConfirmacion();
+			    break;
 			        
-			        //...
-			        default:
-			            dialogo = null;
-			            break;
-			    }
-			 
-			    return dialogo;
+			    //...	
+			default:
+			    dialogo = null;
+			    break;
 		}
+			 
+		return dialogo;
+	}
 	
 	private void goWelcome(){
 		Intent intent=new Intent(this, WelcomeActivity.class);
@@ -140,6 +201,7 @@ public class MainActivity extends Activity {
 		Log.i(INFO, "pantalla maps");
 		startActivity(intent);
 	}
+	
 	private void gpsOn(){
 		// para acceder a los servicios de localizacion
 		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -163,10 +225,13 @@ public class MainActivity extends Activity {
 			Log.d("PRUEBA GPS", "GPS apagado");
 		}
 	}
+	
+	
 	public void activarGps(){
 		Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
 	    startActivity(settingsIntent);		
 	}
+	
 	private Dialog crearDialogoConfirmacion(){
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		 
@@ -190,66 +255,190 @@ public class MainActivity extends Activity {
 	    return builder.create();
 	}
 	
+	private void nuevoEventoCalendario(int duracion, long tiempo, Context ctx){
+		   Intent l_intent = new Intent(Intent.ACTION_EDIT);
+
+		   l_intent.setType("vnd.android.cursor.item/event");
+
+		   //l_intent.putExtra("calendar_id", m_selectedCalendarId);  //this doesn't work
+
+		   l_intent.putExtra("title", "Next ISS pass");
+
+		   l_intent.putExtra("description", "The ISS will pass over you");
+
+		   l_intent.putExtra("eventLocation", "@home");
+
+		   l_intent.putExtra("beginTime", tiempo);
+
+		   l_intent.putExtra("endTime", duracion*1000 + tiempo);
+
+		   l_intent.putExtra("allDay", 1);
+
+		   //status: 0~ tentative; 1~ confirmed; 2~ canceled
+
+		   l_intent.putExtra("eventStatus", 1);
+
+		   //0~ default; 1~ confidential; 2~ private; 3~ public
+
+		   l_intent.putExtra("visibility", 0);
+
+		   //0~ opaque, no timing conflict is allowed; 1~ transparency, allow overlap of scheduling
+
+		   l_intent.putExtra("transparency", 1);
+
+		   //0~ false; 1~ true
+
+		   l_intent.putExtra("hasAlarm", 1);
+
+		   try {
+		       startActivity(l_intent);
+
+		   } catch (Exception e) {
+
+		       Toast.makeText(ctx.getApplicationContext(), "Sorry, no compatible calendar is found!", Toast.LENGTH_LONG).show();
+		   }	
+	}
 	
 
+	private class Proximos extends AsyncTask<String, Integer, Boolean>{
+		
+		private final String PASS_TIME_URL = "http://api.open-notify.org/iss/?n=%s&lat=%s&lon=%s";
+		ArrayList<Pass> pass;
+				
+		private boolean internet = false;
+		int n;
+		double lat, lng;
+		
+		String mensaje;
+		Context context;
+		
+		
+		Proximos(int n, double lat, double lng, Context context)
+		{
+			this.n = n;
+			this.lat = lat;
+			this.lng = lng;
+			this.context = context;
+		}
 
-private MyCalendar m_calendars[];
+		@Override
+		protected Boolean doInBackground(String... params) {			
+			
+			if(this.internet){
+			
+				Log.i("INFO", "Ejecutando task");
+			
+			
+				ArrayList<Pass> passArray = new ArrayList<Pass>();			
+				
+				String formated = String.format(PASS_TIME_URL, Integer.toString(this.n), Double.toString(this.lat), Double.toString(this.lng));
+				
+				
+				Log.i("INFO", "Pre fetch");
+	
+				//realizamos la peticion al servidor
+				HttpClient client = new DefaultHttpClient();
+				HttpGet request = new HttpGet();
+				
+				String responseStr = null;
+	
+				try {
+					request.setURI(new URI(formated));
+					HttpResponse response = client.execute(request);
+					
+					Log.i("INFO", "post fetch");
+	
+					HttpEntity entity = response.getEntity();
+					responseStr = EntityUtils.toString(entity);
+					
+				} catch (Exception e) {
+					
+					this.mensaje = "No se puede contactar con el servidor";
+					return false;
+				}
+				
+				try{
+					JSONObject json = new JSONObject(responseStr);
+		
+					String message = json.getString("message");
+					if(message.equals(message))	{
+						JSONArray positions = json.getJSONArray("response");
+		
+						Log.i("INFO", "parsing JSON");
+						
+						for(int i = 0; i < positions.length(); i++)
+						{
+							JSONObject pos = positions.getJSONObject(i);
+		
+							int duration = Integer.parseInt(pos.getString("duration"));
+							long risetime = Long.parseLong(pos.getString("risetime"));
+		
+							passArray.add(new Pass(duration, risetime));
+		
+						}
+		
+						Log.i("INFO", "fin parse json");
+						
+						this.pass =  passArray;
+		
+						return true;
+		
+					}
+				
+				}catch (Exception e){
+					this.mensaje = "Error al hacer el parse";
+					this.pass = null;
+					return false;
+				}
+				
+				return true;
+				
+			}else{
+				
+				Log.i("INFO", "NO hay conexion");
+				
+				this.pass = new ArrayList<Pass>();
+				
+				this.pass.add(new Pass(601, 1366524444));
+				this.pass.add(new Pass(602, 1366530237));
+				this.pass.add(new Pass(420, 1366536183));
+						
+				return true;				
+			}			
+		}
 
-private void getCalendars() {
 
-    String[] l_projection = new String[]{"_id", "displayName"};
+	    @SuppressLint("NewApi")
+		protected void onPostExecute(Boolean response) {    	
 
-    Uri l_calendars;
+			Log.i("INFO", "POST EXECUTE PASS TIME");
 
-    if (Build.VERSION.SDK_INT >= 8 ) {
+			if(response)
+			{
+				Log.i("INFO", "Tenemos todos los datos, YEAH!" + (Long.toString(this.pass.get(0).risetime)));
+				
+				int NewID = 0 + 1;
+								
+								
+				for(int i = 0; i < this.pass.size(); i++)
+				{
+					Date fecha = new Date(pass.get(i).risetime*1000);
+					long j=pass.get(i).risetime;
+					longs.add(j);
+					duracion.add(pass.get(i).duration);
+					cadenas.add(fecha.toString());
+				}
+				
+				Log.i("INFO", "terminado de poner los eventos");
+				
+				crearAdapter();
 
-        l_calendars = Uri.parse("content://com.android.calendar/calendars");
-
-    } else {
-
-        l_calendars = Uri.parse("content://calendar/calendars");
-
-    }
-
-    Cursor l_managedCursor = this.managedQuery(l_calendars, l_projection, null, null, null);    //all calendars
-
-    //Cursor l_managedCursor = this.managedQuery(l_calendars, l_projection, "selected=1", null, null);   //active calendars
-
-    if (l_managedCursor.moveToFirst()) {
-
-        m_calendars = new MyCalendar[l_managedCursor.getCount()];
-
-        String l_calName;
-
-        String l_calId;
-
-        int l_cnt = 0;
-
-        int l_nameCol = l_managedCursor.getColumnIndex(l_projection[1]);
-
-        int l_idCol = l_managedCursor.getColumnIndex(l_projection[0]);
-
-        do {
-
-            l_calName = l_managedCursor.getString(l_nameCol);
-
-            l_calId = l_managedCursor.getString(l_idCol);
-
-            m_calendars[l_cnt] = new MyCalendar(l_calName, l_calId);
-
-            ++l_cnt;
-
-        } while (l_managedCursor.moveToNext());
-
-    }
-
+			}else{
+				Utils.showError(this.mensaje, this.context);
+			}
+		}	
+	}		
 }
-
-
-
-
-}
-
 
 
 		/**Pass class with the duration and risetime
@@ -269,14 +458,13 @@ private void getCalendars() {
 				this.duration = duration;
 				this.risetime = risetime;
 			}
-		}
-		
+		}		
 		
 		class ErrorOpenNofify extends Exception {
 		    public ErrorOpenNofify(String message) {
 		        super(message);
 		    }
-	}
+		}
 
 
 
